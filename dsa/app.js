d6be04a016448c7305d05774ca9e7a5ac06367b3
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2023-12-08 18:10:28 -0500

    DSA

--- dsa/app.js
+++ dsa/app.js
@@ -61,6 +61,87 @@ const d = "abbba";
 //-------------------------
 // # 121. Best Time to Buy and Sell Stock
 
-function maxProfit(prices) {}
+function maxProfit(prices) {
+  /*   let buy = prices[0]; // initialize to 0-th element, for pointing to lowest buy price
+  let profit = 0;
+
+  if (prices.length === 0) {
+    return 0;
+  }
+
+  for (let i = 1; i < prices.length; i++) {
+    // start loop at 1-th element
+    let compare = prices[i] - buy;
+
+    if (compare > profit) {
+      profit = compare;
+    }
+    if (prices[i] < buy) {
+      buy = prices[i];
+    }
+  }
+  return profit; */
+
+  let buy = prices[0];
+  let profit = 0;
+
+  for (i = 1; i < prices.length; i++) {
+    if (prices[i] > buy) {
+      // avoids operating on lower values to var 'buy'
+      // check if price at new index is greater than buy
+      let diff = prices[i] - buy;
+
+      profit = Math.max(diff, profit);
+    } else {
+      buy = prices[i];
+    }
+  }
+  return profit;
+}
 
 const prices = [7, 1, 5, 3, 6, 4];
+// console.log(maxProfit(prices));
+
+function binSearch(nums, target) {
+  /* 
+  Find middle using the 2 pointers, compare its value with target 
+    If target is less, move 'high' to middle-1 ;  find the sorted array (characteristic = low value and high value) and check within
+    If target is more, move 'low' to middle; if 
+  */
+
+  // pointers
+  let low = 0;
+  let high = nums.length - 1;
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    }
+
+    /*     find sorted array first */
+
+    if (nums[low] <= nums[mid]) {
+      // sorted array in left side
+      if (target >= nums[low] && target < nums[mid]) {
+        high = mid - 1;
+      } else {
+        // TODO:  not sure when this would be possible
+        low = mid + 1;
+      }
+    } else {
+      // sorted array in right side
+      if (target > nums[mid] && target <= nums[high]) {
+        low = mid + 1;
+      } else {
+        high = mid - 1;
+      }
+    }
+  }
+  return -1;
+}
+
+const nums = [4, 5, 6, 7, 0, 1, 2];
+const target = 1;
+console.log(binSearch(nums, target));
